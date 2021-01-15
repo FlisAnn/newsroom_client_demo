fdd2cd9576d347875dd453c080bdb4ed0224dff2
flis_ann <ann.flismark@gmail.com>
2021-01-15 14:26:22 +0100

    Adding styling to loginform

--- app/components/LoginScreen.jsx
+++ app/components/LoginScreen.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from "react";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
-    <View>
+    <View style={{ flex: 1, margin: 10 }}>
       <Text>Login screen</Text>
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        onChangeText={() => setEmail(text)}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        onChangeText={() => setPassword(text)}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default LoginScreen
+export default LoginScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+    height: 40,
+    padding: 20,
+    borderColor: "gray",
+    borderWidth: 1,
+  },
+});
