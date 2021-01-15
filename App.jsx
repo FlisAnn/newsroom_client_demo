fe32367a054c7056ea68b6cf31491b03a3649b2f
flis_ann <ann.flismark@gmail.com>
2021-01-15 13:47:39 +0100

    adding new component

--- App.jsx
+++ App.jsx
@@ -4,11 +4,12 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import MainScreen from "./app/components/MainScreen";
 import ArticleShow from "./app/components/ArticleShow";
+import { Button } from "react-native";
 
 const Stack = createStackNavigator();
 
 const App = () => {
-  const { appTitle } = useSelector((state) => state);
+  const { appTitle } = useSelector(state => state);
   return (
     <NavigationContainer>
       <Stack.Navigator>
@@ -16,6 +17,9 @@ const App = () => {
           name="Start"
           component={MainScreen}
           options={{
+            headerRight: () => {
+             return <Button title="Click me" />;
+            },
             title: appTitle,
             headerStyle: {
               backgroundColor: "black",
@@ -26,7 +30,7 @@ const App = () => {
             },
           }}
         />
-        <Stack.Screen name="ArticleShow" component={ArticleShow}/>
+        <Stack.Screen name="ArticleShow" component={ArticleShow} />
       </Stack.Navigator>
     </NavigationContainer>
   );
